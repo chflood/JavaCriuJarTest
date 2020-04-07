bcebb27c99aaf8781f36c0953e881f2499c84bb1
chflood <chf@redhat.com>
2020-04-07 16:04:06 -0400

    Add files via upload

---
+++ TestRandom.java
@@ -0,0 +1,60 @@
+import org.checkpoint.*;
+
+
+class BeforeHook extends Hook {
+    String str;
+    BeforeHook(String s) {
+	str = s;
+    }
+	
+    public void run() {
+	System.out.println("Hook:Before Checkpoint: " + str);
+    }
+}
+
+class AfterHook extends Hook {
+    String str;
+    AfterHook(String s) {
+	str = s;
+    }
+
+    public void run() {
+	System.out.println("Hook:After Restore: " + str);
+    }
+}
+
+
+public class TestRandom {
+    
+    public static void  main(String[] args) {
+        if (args.length != 2) System.out.println("Usage: please provide upper bound and sample size");
+        int upperBound = Integer.parseInt(args[0]);        
+        long sampleSize = Long.parseLong(args[1]);
+        long testarray[] = new long[upperBound];
+
+        for (long i = 0; i < sampleSize; i++) {
+            Double r = Math.random() * upperBound;
+            int v = (int)  Math.floor(r);
+            testarray[v] += 1;
+        }
+	//	CheckpointRestore.CheckTheWorld();
+	long end = System.currentTimeMillis();
+	String dir = "/home/chf/SavedWorlds/run" + end;
+	
+	CheckpointRestore.RegisterCheckpointHook(new BeforeHook("Restore Command: As root java -cp ./:./checkpoint.jar -XX:+UseSerialGC -XX:-UsePerfData TestRandomRestore " + dir ));
+	CheckpointRestore.RegisterRestoreHook(new AfterHook("That's all folks"));	
+	CheckpointRestore.SaveTheWorld(dir);
+	
+        long max = 0; long min = upperBound; long average = 0;
+        for (int i = 0; i < upperBound; i++) {
+            long s = testarray[i];
+            if (s > max) { max = s;}
+            if (s < min) { min = s;}
+            average = average + s;
+        }
+        double median = average / upperBound;
+
+        System.out.println("Testing random number generator with upper bound = " + upperBound + " and sample size " + sampleSize);
+        System.out.println("Max bin size = " + max + " Min bin size = " + min + " median bin size = " + median);
+    }
+}
---
+++ TestRandomRestore.java
@@ -0,0 +1,13 @@
+import org.checkpoint.*;
+
+public class TestRandomRestore {
+
+    public static void main(String[] args) {
+	if (args.length != 1) System.out.println("Usage: please provide checkpoint directory");
+
+	//	CheckpointRestore.CheckTheWorld();
+	CheckpointRestore.RestoreTheWorld(args[0]);
+
+	System.out.println("Done!");
+    }
+}
